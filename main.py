f2054bd099d79e6ab1aa9eb8d64fc9384aa8334c
Greensky <david.hesliere@gmail.com>
2022-11-22 21:02:22 +0100

    chore: :construction: Work on script (need opti)

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-from turtle import turtle
+from turtle import *
+from PIL import Image;
+
+img = Image.open('image.jpg').convert('RGB')
+
+colormode(255)
+setworldcoordinates(-1, img.height, img.width, -1)
+pendown()
+speed(500)
+
+for y in range(img.height):
+    for x in range(img.width):
+        pendown()
+        goto(x, y)
+        color(img.getpixel((x, y)))
+        forward(1)
+    penup()
+    goto(0, x)
+
+done()
