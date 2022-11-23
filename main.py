dbb097879035a6012dedcf550d4dbafc8938fa07
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-11-23 09:30:30 +0100

    perf: :zap: Trying to improve speed

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from PIL import Image;
 
 img = Image.open('image.jpg').convert('RGB')
 
-colormode(255)
-setworldcoordinates(-1, img.height, img.width, -1)
-pendown()
-speed(500)
+
+all_pixels = []
 
 for y in range(img.height):
     for x in range(img.width):
-        pendown()
+        all_pixels.append(img.getpixel((x, y)));
+
+print("loading finished")
+colormode(255)
+setworldcoordinates(-1, img.height, img.width, -1)
+pendown()
+
+for y in range(img.height):
+    for x in range(img.width):
         goto(x, y)
-        color(img.getpixel((x, y)))
+        color(all_pixels[x + y])
         forward(1)
     penup()
     goto(0, x)
+    pendown()
 
 done()
